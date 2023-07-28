ceb3170e3ebc1d3c6e7b1bab4635a4b94e2e5a80
vishwjeet thakur <ervishwjeetthakur@gmail.com>
2023-07-28 13:43:10 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { Todo } from "./models/Todo.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("<h1>hello world</h1>");
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/mern-todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to data base");
+  })
+  .catch((err) => {
+    console.log("something went wrong: " + err);
+  });
+
+
+app.get('/todos' ,async (req, res) => {
+   const data = await Todo.find();
+   return res.status(200).json(data);
+})
+
+app.post('/todo/new', async (req, res) =>{
+    const todo = new Todo({
+        task : req.body.text,
+    })
+    try{
+        const data = await todo.save();
+        return res.status(200).json("added");
+    }catch(err){
+       return res.status(500).json(err.message);
+    }
+})
+
+app.delete("/todo/delete/:id",async (req,res)=>{
+    try{
+    console.log(req.params.id);
+    const deleteTask = await Todo.findByIdAndDelete(req.params.id);
+    return res.status(200).json("deleted successfully");
+    }catch(err){
+    return res.status(500).json(err.message);
+    }
+})
+
+app.put("/todo/complete/:id", async (req, res)=>{
+    try{
+        const getTask = await Todo.findById(req.params.id);
+
+        if(!getTask)return res.status(404).json("task not found");
+        //update that Task
+        getTask.completed = !getTask.completed;
+
+        const updatedTask = await getTask.save();
+        return res.status(200).json("task updated successfully");
+    }catch(err){
+        return res.status(500).json(err.message);
+    }
+})
+app.listen(5500, () => {
+  console.log("port is running on posrt no 5500");
+});
---
+++ models/Todo.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+
+const TodoSchema = new Schema({
+    task:{
+        type : String,
+        required: true,
+        min_length : 5
+    },
+    completed:{
+        type : Boolean,
+        default: false
+    },
+    timestamp:{
+        type : String,
+        default: Date.now()
+    }
+})
+
+export const Todo = mongoose.model('TodoList', TodoSchema);
+
