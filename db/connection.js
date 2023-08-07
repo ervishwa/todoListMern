a497498831cfc11f0111c54bb4613fcd08341cb7
vishwjeet thakur <ervishwjeetthakur@gmail.com>
2023-08-07 13:59:18 +0530

    database time out increases

--- db/connection.js
+++ db/connection.js
@@ -8,6 +8,7 @@ const Db = process.env.MongoDbUrl;
 mongoose.connect(`${Db}`,{
     useNewUrlParser: true,
    useUnifiedTopology: true,
+   bufferTimeoutMS: 30000,
 }).then(()=>{
     console.log("database connection established")
 }).catch(err=>console.log("Error Occured " + err));
